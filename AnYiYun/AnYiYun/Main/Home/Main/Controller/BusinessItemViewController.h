a72d81200ca6582b051252541d2d66ea67ba5379
吴宛茹_mac <925992894@qq.com>
2017-07-25 21:46:49 +0800

    增加全部模块入口

---
+++ AnYiYun/AnYiYun/Main/Home/Main/Controller/BusinessItemViewController.h
@@ -0,0 +1,16 @@
+//
+//  BusinessItemViewController.h
+//  AnYiYun
+//
+//  Created by wwr on 2017/7/25.
+//  Copyright © 2017年 wwr. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+/**
+ 全部模块入口
+ */
+@interface BusinessItemViewController : BaseViewController
+
+@end
---
+++ AnYiYun/AnYiYun/Main/Home/Main/Controller/BusinessItemViewController.m
@@ -0,0 +1,39 @@
+//
+//  BusinessItemViewController.m
+//  AnYiYun
+//
+//  Created by wwr on 2017/7/25.
+//  Copyright © 2017年 wwr. All rights reserved.
+//
+
+#import "BusinessItemViewController.h"
+
+@interface BusinessItemViewController ()
+
+@end
+
+@implementation BusinessItemViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.title = @"全部应用";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
