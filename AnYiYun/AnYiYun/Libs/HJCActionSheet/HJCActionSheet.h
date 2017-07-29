bda143c414c8fd3306a5de0f0a7de12a93248be1
吴宛茹_mac <925992894@qq.com>
2017-07-30 00:03:51 +0800

    完善设备信息页和定位

---
+++ AnYiYun/AnYiYun/Libs/HJCActionSheet/HJCActionSheet.h
@@ -0,0 +1,38 @@
+//
+//  HJCActionSheet.h
+//  wash
+//
+//  Created by weixikeji on 15/5/11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class HJCActionSheet;
+
+@protocol HJCActionSheetDelegate <NSObject>
+
+@optional
+
+/**
+ *  点击按钮
+ */
+- (void)actionSheet:(HJCActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex;
+
+@end
+
+@interface HJCActionSheet : UIView
+
+/**
+ *  代理
+ */
+@property (nonatomic, weak) id <HJCActionSheetDelegate> delegate;
+
+/**
+ *  创建对象方法
+ */
+- (instancetype)initWithDelegate:(id<HJCActionSheetDelegate>)delegate andMessageTitle:(NSString *)messageTitle  CancelTitle:(NSString *)cancelTitle OtherTitles:(NSString*)otherTitles,... NS_REQUIRES_NIL_TERMINATION;
+
+- (void)show;
+
+@end
